d367c7f1342d8f3d3485d5e362cd1d59311b4906
ilgarguliyevv <ilgarguliyev2004>
2024-04-01 01:42:32 +0000

    jstask

---
+++ app.js
@@ -0,0 +1,39 @@
+// let bank = 1;
+
+// if (bank == 1) {
+//   console.log("balans");
+// } else if (bank == 2) {
+//   console.log("medaxil+balans");
+// } else if (bank == 3) {
+//   console.log("balansdan pul cixarildi");
+// } else if (bank == 4) {
+//   console.log("cixis");
+// } else {
+//   console.log("Error");
+// }
+
+// let num1 = 5;
+// let num2 = 6;
+// let num3 = 7;
+
+// if (num1 < num2 < num3) {
+//   console.log("num1 num2 num3");
+// }
+
+// let age = 19;
+// let money = 10;
+
+// if (age > 18 && money == 10) {
+//   console.log("welcome");
+// }
+// if (age == 18 && money >= 30) {
+//   console.log("hello");
+// }
+
+// let num1 = 5;
+// let num2 = 6;
+// let num3 = 7;
+
+// if (num1 < num2 < num3) {
+//   console.log(num3);
+// }
