253a36fa8e7bdb08cb22fc36523bf32b4840f8d0
ilgarguliyevv <ilgarguliyev2004@gmail.com>
2024-04-02 17:11:25 +0400

    02.04 gurban

---
+++ JsTask/JsIntro/app.js
@@ -0,0 +1,12 @@
+let fruit = prompt("Please choose valid fruit name")
+
+let kg = prompt("Kiloqrami daxil edin")
+
+switch (fruit) {
+    case "alma":
+        console.log("alma");
+        break;
+
+    default:
+        break;
+}
