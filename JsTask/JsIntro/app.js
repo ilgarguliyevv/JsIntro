371326297040e9240104782df62103f15101c7e5
ilgarguliyevv <ilgarguliyev2004@gmail.com>
2024-04-02 19:15:14 +0400

    final task

--- JsTask/JsIntro/app.js
+++ JsTask/JsIntro/app.js
@@ -1,12 +1,31 @@
-let fruit = prompt("Please choose valid fruit name")
+// let fruit = prompt("Please choose valid fruit name")
 
-let kg = prompt("Kiloqrami daxil edin")
+// let kg = prompt("Kiloqrami daxil edin")
 
-switch (fruit) {
-    case "alma":
-        console.log("alma");
-        break;
+// switch (fruit) {
+//     case "alma":
+//         console.log("alma");
+//         break;
 
-    default:
-        break;
-}
+//     default:
+//         break;
+// }
+
+// let arr = ("12", 5, 3, -4, -5, 6, -7, "99", NaN, undefined, true, false, null);
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] * 1 == arr[i] && arr[i] == "number") {
+//         console.log(arr[i]);
+//     }
+// }
+
+// let arr1 = (12, 5, 3, -4, -5, -6, -7, 99)
+// let max = arr1[0];
+
+// for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] > max) {
+//         max = arr1[i];
+//     }
+// }
+// console.log(arr1[i]);
+
